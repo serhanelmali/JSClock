42eafcfa91955a4e9d6d9b0a2aefff7ca308a33e
Serhan Elmalı <serhanelmali@Serhan-MacBook-Pro.local>
2021-02-17 16:12:24 +0300

    bugs fixed

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  
-  const secondsDegrees = ((seconds / 60) * 360) +90;
+  const secondsDegrees = ((seconds / 60) * 360) + 90
+  secondHand.style.transition = `${seconds === 60} ?  none : all .5s`
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) +90;
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transition = `${seconds} === 60 ?  none : all 0.05s`
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-
-  const hour = now.getMinutes();
-  const hourDegrees = ((mins / 12) * 360) + 90;
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transition = `${seconds} === 60 ?  none : all 0.05s`
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-  console.log({seconds,secondsDegrees})
-
 }
+
 setInterval(setDate, 1000);
+
